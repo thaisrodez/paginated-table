30e29d6a923c1977a82e596a0217243899ee6d87
thais de rodez <thaisderodez@gmail.com>
2023-06-30 15:30:14 +0200

    feat: install rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,37 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+
+import packageJson from "./package.json" assert { type: "json" };
+
+// Rollup configuration file, which is used to bundle a React component library created with Typescript.
+
+export default [
+  {
+    // library entry point
+    input: "src/index.ts",
+    // use both ESM and commonJS modules to distribute our library
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+  {
+    // determines how types in the library are distributed and it uses dts plugin to do it.
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+];
