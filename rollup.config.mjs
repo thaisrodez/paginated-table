390204327ebabf20992d43d33ac1160f7e53530f
thais de rodez <thaisderodez@gmail.com>
2023-07-06 10:18:19 +0100

    fix(rollup): import postcss

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
 
 import packageJson from "./package.json" assert { type: "json" };
 
